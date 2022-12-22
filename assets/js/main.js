e78141cd1c91d41d614c3a312829453c47d48288
MathildeCornelis <mathilde.cornelis26@gmail.com>
2022-12-22 11:51:22 +0100

    css + responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,8 @@ async function catchData() {
     
     // mettre les infos dan les différents paragraphes
     function createCarte() {
-
+        let div = document.getElementById('one');
+        div.style.display = "block";
         let date = document.getElementsByClassName('date')[0];
         date.textContent = stock.list[0].dt_txt;
     
@@ -21,7 +22,7 @@ async function catchData() {
         nameCity.textContent = stock.city.name;
         
         let tempGen = document.getElementsByClassName('tempGen')[0];
-        tempGen.textContent = moyenne() + '°C';
+        tempGen.textContent = Math.round(stock.list[0].main.temp) + '°C';
     
         let tempMin = document.getElementsByClassName('tempMin')[0];
         tempMin.textContent = Math.round(stock.list[0].main.temp_min) + '°C';
@@ -33,80 +34,119 @@ async function catchData() {
         desc.textContent = stock.list[0].weather[0].description;
     
         let icon = document.getElementsByClassName('icon')[0];
-        icon.textContent = stock.list[0].weather[0].icon;
+        icon.src = "http://openweathermap.org/img/wn/" + stock.list[0].weather[0].icon + "@2x.png";
     }
     createCarte();
 
-    //faire la moyenne des t°
-    function moyenne() {
-        let somme = 0;
-        let total = 0;
+    function createSecondCarte() {
+        let div = document.getElementById('two');
+        div.style.display = "block";
 
-        for (let i = 0; i < stock.list.length; i++) {
-            let temperatures = stock.list[i].main.temp;
-
-            somme += temperatures;
-
-            total++;
-        }
-
-        let moyenne = Math.round(somme / total);
-
-        return moyenne;
+        let date = document.getElementsByClassName('date')[1];
+        date.textContent = stock.list[8].dt_txt;
+    
+        let nameCity = document.getElementsByClassName('name')[1];
+        nameCity.textContent = stock.city.name;
+        
+        let tempGen = document.getElementsByClassName('tempGen')[1];
+        tempGen.textContent = Math.round(stock.list[8].main.temp) + '°C';
+    
+        let tempMin = document.getElementsByClassName('tempMin')[1];
+        tempMin.textContent = Math.round(stock.list[8].main.temp_min) + '°C';
+    
+        let tempMax = document.getElementsByClassName('tempMax')[1];
+        tempMax.textContent = Math.round(stock.list[8].main.temp_max) + '°C';
+    
+        let desc = document.getElementsByClassName('desc')[1];
+        desc.textContent = stock.list[8].weather[0].description;
+    
+        let icon = document.getElementsByClassName('icon')[1];
+        icon.src = "http://openweathermap.org/img/wn/" + stock.list[8].weather[0].icon + "@2x.png";
     }
+    createSecondCarte();
+
+    function createThirdCarte() {
+        let div = document.getElementById('three');
+        div.style.display = "block";
+
+        let date = document.getElementsByClassName('date')[2];
+        date.textContent = stock.list[16].dt_txt;
     
-
-
-
-    // function createSecondCarte() {
-
-    //     let date = document.getElementsByClassName('date')[1];
-    //     date.textContent = stock.list[4].dt_txt;
-    
-    //     let nameCity = document.getElementsByClassName('name')[1];
-    //     nameCity.textContent = stock.city.name;
+        let nameCity = document.getElementsByClassName('name')[2];
+        nameCity.textContent = stock.city.name;
         
-    //     let tempGen = document.getElementsByClassName('tempGen')[1];
-    //     tempGen.textContent = stock.list[4].main.temp;
+        let tempGen = document.getElementsByClassName('tempGen')[2];
+        tempGen.textContent = Math.round(stock.list[16].main.temp) + '°C';
     
-    //     let tempMin = document.getElementsByClassName('tempMin')[1];
-    //     tempMin.textContent = stock.list[4].main.temp_min;
+        let tempMin = document.getElementsByClassName('tempMin')[2];
+        tempMin.textContent = Math.round(stock.list[16].main.temp_min) + '°C';
     
-    //     let tempMax = document.getElementsByClassName('tempMax')[1];
-    //     tempMax.textContent = stock.list[4].main.temp_max;
+        let tempMax = document.getElementsByClassName('tempMax')[2];
+        tempMax.textContent = Math.round(stock.list[16].main.temp_max) + '°C';
     
-    //     let desc = document.getElementsByClassName('desc')[1];
-    //     desc.textContent = stock.list[4].weather[0].description;
+        let desc = document.getElementsByClassName('desc')[2];
+        desc.textContent = stock.list[16].weather[0].description;
     
-    //     let icon = document.getElementsByClassName('icon')[1];
-    //     icon.textContent = stock.list[4].weather[0].icon;
-    // }
-    // createSecondCarte();
+        let icon = document.getElementsByClassName('icon')[2];
+        icon.src = "http://openweathermap.org/img/wn/" + stock.list[16].weather[0].icon + "@2x.png";
+    }
+    createThirdCarte();
 
-    // function createThirdCarte() {
+    function createFourthCarte() {
+        let div = document.getElementById('four');
+        div.style.display = "block";
 
-    //     let date = document.getElementsByClassName('date')[2];
-    //     date.textContent = stock.list[4].dt_txt;
+        let date = document.getElementsByClassName('date')[3];
+        date.textContent = stock.list[24].dt_txt;
     
-    //     let nameCity = document.getElementsByClassName('name')[1];
-    //     nameCity.textContent = stock.city.name;
+        let nameCity = document.getElementsByClassName('name')[3];
+        nameCity.textContent = stock.city.name;
         
-    //     let tempGen = document.getElementsByClassName('tempGen')[1];
-    //     tempGen.textContent = stock.list[4].main.temp;
+        let tempGen = document.getElementsByClassName('tempGen')[3];
+        tempGen.textContent = Math.round(stock.list[24].main.temp) + '°C';
     
-    //     let tempMin = document.getElementsByClassName('tempMin')[1];
-    //     tempMin.textContent = stock.list[4].main.temp_min;
+        let tempMin = document.getElementsByClassName('tempMin')[3];
+        tempMin.textContent = Math.round(stock.list[24].main.temp_min) + '°C';
     
-    //     let tempMax = document.getElementsByClassName('tempMax')[1];
-    //     tempMax.textContent = stock.list[4].main.temp_max;
+        let tempMax = document.getElementsByClassName('tempMax')[3];
+        tempMax.textContent = Math.round(stock.list[24].main.temp_max) + '°C';
     
-    //     let desc = document.getElementsByClassName('desc')[1];
-    //     desc.textContent = stock.list[4].weather[0].description;
+        let desc = document.getElementsByClassName('desc')[3];
+        desc.textContent = stock.list[24].weather[0].description;
     
-    //     let icon = document.getElementsByClassName('icon')[1];
-    //     icon.textContent = stock.list[4].weather[0].icon;
-    // }
+        let icon = document.getElementsByClassName('icon')[3];
+        icon.src = "http://openweathermap.org/img/wn/" + stock.list[24].weather[0].icon + "@2x.png";
+    }
+    createFourthCarte();
+
+    function createFifthCarte() {
+        let div = document.getElementById('five');
+        div.style.display = "block";
+
+        let date = document.getElementsByClassName('date')[4];
+        date.textContent = stock.list[32].dt_txt;
     
+        let nameCity = document.getElementsByClassName('name')[4];
+        nameCity.textContent = stock.city.name;
+        
+        let tempGen = document.getElementsByClassName('tempGen')[4];
+        tempGen.textContent = Math.round(stock.list[32].main.temp) + '°C';
+    
+        let tempMin = document.getElementsByClassName('tempMin')[4];
+        tempMin.textContent = Math.round(stock.list[32].main.temp_min) + '°C';
+    
+        let tempMax = document.getElementsByClassName('tempMax')[4];
+        tempMax.textContent = Math.round(stock.list[32].main.temp_max) + '°C';
+    
+        let desc = document.getElementsByClassName('desc')[4];
+        desc.textContent = stock.list[32].weather[0].description;
+    
+        let icon = document.getElementsByClassName('icon')[4];
+        icon.src = "http://openweathermap.org/img/wn/" + stock.list[32].weather[0].icon + "@2x.png";
+    }
+    createFifthCarte();
+
+
 };
 
 document.getElementById('submit').addEventListener('click', function(){catchData()})
